8ea5928a02aec246d15cbbae920956f6a33696ed
anuragmis2022-del <anuragmis2022@gmail.com>
2026-06-14 17:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species)
+	{
+		this.species= species
+		
+	}
+	getspecies()
+	{
+				return this.species
+			}
+	makeSound()
+	{
+		console.log(`${this.species} makes a sound`)
+	}
+	
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor (species)
+	{
+	super(species)
+	}
+	bark()
+	{
+	console.log(`woof`)
+	}
+}
 
-class Cat extends Animal {}
+
+class Cat extends Animal {
+constructor()
+{
+super(species)
+
+}
+purr()
+{
+console.log("purr")
+}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
